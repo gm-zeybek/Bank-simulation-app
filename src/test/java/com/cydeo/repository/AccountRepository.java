b921434120de50ea7c6d10ea3b53fbe263fc6400
murat <murat.zeybek@sky.uk>
2023-09-10 11:55:37 +0100

    started setup

---
+++ src/test/java/com/cydeo/repository/AccountRepository.java
@@ -0,0 +1,15 @@
+package com.cydeo.repository;
+
+import com.cydeo.model.Account;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+@Component
+public class AccountRepository {
+   public static List<Account> accountList = new ArrayList<>();
+   public Account saveAccount(Account account){
+        accountList.add(account);
+        return account;
+    }
+}
---
+++ src/test/java/com/cydeo/service/Impl/AccountServiceImpl.java
@@ -0,0 +1,33 @@
+package com.cydeo.service.Impl;
+
+import com.cydeo.enums.AccountType;
+import com.cydeo.model.Account;
+import com.cydeo.repository.AccountRepository;
+import com.cydeo.service.AccountService;
+import org.springframework.stereotype.Component;
+
+import java.math.BigDecimal;
+import java.util.Date;
+import java.util.List;
+import java.util.UUID;
+@Component
+public class AccountServiceImpl implements AccountService {
+    AccountRepository accountRepository;
+
+    public AccountServiceImpl(AccountRepository accountRepository) {
+        this.accountRepository = accountRepository;
+    }
+
+    @Override
+    public Account createAccount(BigDecimal balance, Date createDate, AccountType accountType, Long userId) {
+        // we need to create an object
+        Account account = Account.builder().id(UUID.randomUUID()).balance(balance).creationDate(createDate).accountType(accountType).userId(userId).build();
+        // saving and returning that account
+        return accountRepository.saveAccount(account);
+    }
+
+    @Override
+    public List<Account> listAllAccount() {
+        return null;
+    }
+}
