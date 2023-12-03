f164c2a4af1d0e3409b8de329c9c86f9446865e6
murat <murat.zeybek@sky.uk>
2023-12-03 14:01:29 +0000

    Entities are created

---
+++ src/main/java/com/cydeo/repository/entity/Account.java
@@ -0,0 +1,32 @@
+package com.cydeo.repository.entity;
+
+import com.cydeo.enums.AccountStatus;
+import com.cydeo.enums.AccountType;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.util.Date;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Table(name = "accounts")
+@Entity
+public class Account {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private BigDecimal balance;
+    @Enumerated(EnumType.STRING)
+    private AccountType accountType;
+    @Column(columnDefinition = "TIMESTAMP")
+    private Date creationDate;
+
+    private Long userId;
+    @Enumerated(EnumType.STRING)
+    private AccountStatus accountStatus;
+}
---
+++ src/main/java/com/cydeo/repository/entity/Transaction.java
@@ -0,0 +1,36 @@
+package com.cydeo.repository.entity;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Positive;
+import javax.validation.constraints.Size;
+import java.math.BigDecimal;
+import java.util.Date;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Table(name = "transactions")
+public class Transaction {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @ManyToOne
+    private Account sender;
+    @ManyToOne
+    private Account receiver;
+
+    private BigDecimal amount;
+
+    private String message;
+    @Column(columnDefinition = "TIMESTAMP")
+    private Date createDate;
+}
